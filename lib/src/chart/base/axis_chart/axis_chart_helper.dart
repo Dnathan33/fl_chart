18f42ad9d649a6983eee956ed111a74a76fb82b8
Nathenael Dereb <dereb@adobe.com>
2022-05-20 10:15:01 -0700

    adds max value only when it aligns with interval

--- lib/src/chart/base/axis_chart/axis_chart_helper.dart
+++ lib/src/chart/base/axis_chart/axis_chart_helper.dart
@@ -26,8 +26,7 @@ class AxisChartHelper {
     required double baseLine,
     required double interval,
   }) sync* {
-    final initialValue = Utils()
-        .getBestInitialIntervalValue(min, max, interval, baseline: baseLine);
+    final initialValue = Utils().getBestInitialIntervalValue(min, max, interval, baseline: baseLine);
     var axisSeek = initialValue;
     final firstPositionOverlapsWithMin = axisSeek == min;
     if (!minIncluded && firstPositionOverlapsWithMin) {
@@ -37,8 +36,7 @@ class AxisChartHelper {
     final count = diff ~/ interval;
     final lastPosition = initialValue + (count * interval);
     final lastPositionOverlapsWithMax = lastPosition == max;
-    final end =
-        !maxIncluded && lastPositionOverlapsWithMax ? max - interval : max;
+    final end = !maxIncluded && lastPositionOverlapsWithMax ? max - interval : max;
 
     final epsilon = interval / 100000;
     if (minIncluded && !firstPositionOverlapsWithMin) {
@@ -48,7 +46,7 @@ class AxisChartHelper {
       yield axisSeek;
       axisSeek += interval;
     }
-    if (maxIncluded && !lastPositionOverlapsWithMax) {
+    if (maxIncluded && lastPositionOverlapsWithMax) {
       yield max;
     }
   }
